f1d395046c01434f82bb8482dc4ebbd03c9dea93
tatsupiko <identity2022@outlook.jp>
2023-07-20 23:31:46 +0900

    [Update] 不要なプロパティを削除した。

--- Assets/Node.cs
+++ Assets/Node.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 // 日本語対応
 public class Node : MonoBehaviour
 {
-    public static int NodeSize { get; set; } = 32;
     public Node Parent { get; set; } = null;
-    public Vector2 Position { get; set; } = Vector2.zero;
-    public Vector2 Center { get; set; } = Vector2.zero;
+    
 
     private enum NodeType
     {
