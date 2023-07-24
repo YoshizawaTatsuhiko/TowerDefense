b39d72d89e4743ac63b388f2315e3ba85a316ccf
tatsupiko <identity2022@outlook.jp>
2023-07-24 22:46:25 +0900

    [Update] A*の進捗を進めた。

--- Assets/AStar.cs
+++ Assets/AStar.cs
@@ -5,5 +5,12 @@ using UnityEngine;
 // 日本語対応
 public class AStar : MonoBehaviour
 {
-    
+    [SerializeField] private int _row = 5;
+    [SerializeField] private int _column = 5;
+    private Cell[,] _cells = null;
+
+    private void Start()
+    {
+        _cells = new Cell[ _row, _column ];
+    }
 }
--- Assets/Cell.cs
+++ Assets/Cell.cs
@@ -2,14 +2,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // 日本語対応
-public class Grid : MonoBehaviour
+public class Cell : MonoBehaviour
 {
+    public Vector2 coodinate { get; set; } = Vector2.zero;
     public float TotalCost { get; set; } = 0f;  // 実コスト + 推定コスト
-    public int ActualCost { get; set; } = 0;  // 実コスト
+    public float ActualCost { get; set; } = 0;  // 実コスト
     public float HeuristicCost { get; set; } = 0f;  // 推定コスト
-    public GridState State { get; set; } = GridState.None;  // セルの状態
+    public CellState State { get; set; } = CellState.None;  // セルの状態
 
-    public enum GridState
+    public enum CellState
     {
         None        = 0,
         Open        = 1,
