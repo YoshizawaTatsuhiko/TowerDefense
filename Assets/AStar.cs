0c0f6e2ecb181c2160d86742e7fa983d4860569e
tatsupiko <identity2022@outlook.jp>
2023-07-28 23:55:34 +0900

    [Update] A*アルゴリズムにあるCell型の2次元配列を初期化する処理を追加した？

--- Assets/AStar.cs
+++ Assets/AStar.cs
@@ -7,10 +7,20 @@ public class AStar : MonoBehaviour
 {
     [SerializeField] private int _row = 5;
     [SerializeField] private int _column = 5;
+    [SerializeField] private Cell _floorTile = null;
     private Cell[,] _cells = null;
 
     private void Start()
     {
         _cells = new Cell[ _row, _column ];
+
+        for (int y = 0; y < _row; y++)
+        {
+            for (int x = 0; x < _column; x++)
+            {
+                _cells[y, x] = Instantiate(_floorTile, 
+                    new Vector2(x - (_column / 2f), y - (_row / 2f)), Quaternion.identity, transform);
+            }
+        }
     }
 }
