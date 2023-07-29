5b625517f71a6172493ad82230a16640cb51cf72
tatsupiko <identity2022@outlook.jp>
2023-07-29 15:43:08 +0900

    [Update] CellStateをクラスの外に移動させた。

--- Assets/Cell.cs
+++ Assets/Cell.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 // 日本語対応
 public class Cell : MonoBehaviour
 {
-    public Vector2 coodinate { get; set; } = Vector2.zero;
     public float TotalCost { get; set; } = 0f;  // 実コスト + 推定コスト
-    public float ActualCost { get; set; } = 0;  // 実コスト
+    public float ActualCost { get; set; } = 0f;  // 実コスト
     public float HeuristicCost { get; set; } = 0f;  // 推定コスト
     public CellState State { get; set; } = CellState.None;  // セルの状態
+}
 
-    public enum CellState
-    {
-        None        = 0,
-        Open        = 1,
-        Close       = 2,
-        CannotOpen  = 4,
-    }
+public enum CellState
+{
+    None = 0,
+    Open = 1,
+    Close = 2,
+    CannotOpen = 4,
 }
