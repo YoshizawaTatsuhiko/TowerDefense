d23ed5be54aa751b76284a207506afb86ab844fc
tatsupiko <identity2022@outlook.jp>
2023-08-03 23:57:24 +0900

    [Update] Cellが自身を開いた親を記憶できるようにした。

--- Assets/Cell.cs
+++ Assets/Cell.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 // 日本語対応
 public class Cell : MonoBehaviour
 {
+    public Cell Parent { get; set; } = null;
     public float TotalCost { get; set; } = 0f;  // 実コスト + 推定コスト
     public float ActualCost { get; set; } = 0f;  // 実コスト
     public float HeuristicCost { get; set; } = 0f;  // 推定コスト
-    public CellState State => _cellState;  // セルの状態
+    public CellState State { get => _cellState; set => _cellState = value; }  // セルの状態
 
     [SerializeField ] private CellState _cellState = CellState.None;
 }
