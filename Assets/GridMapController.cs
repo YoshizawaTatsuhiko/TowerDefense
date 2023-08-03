57185cda69460061708f7755f73e30a41a9b9391
tatsupiko <identity2022@outlook.jp>
2023-08-03 23:59:32 +0900

    [Update] 作成されたマップの情報に応じて、設置するオブジェクトを変更する処理を追加した。

--- Assets/GridMapController.cs
+++ Assets/GridMapController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 // 日本語対応
@@ -7,19 +8,35 @@ public class GridMapController : MonoBehaviour
 {
     [SerializeField] private int _row = 5;
     [SerializeField] private int _column = 5;
+    [SerializeField] private Cell _wallTile = null;
     [SerializeField] private Cell _floorTile = null;
     private Cell[,] _cells = null;
 
     private void Start()
     {
         _cells = new Cell[ _row, _column ];
+        var maze = new HoleDigging(_row, _column);
+        Cell cell = null;
 
-        for (int r = 0; r < _row; r++)
+        for (int i = 0; i < maze.GetHeight(); i++)
         {
-            for (int c = 0; c < _column; c++)
+            for (int j = 0; j < maze.GetWidth(); j++)
             {
-                _cells[r, c] = Instantiate(_floorTile,
-                    new Vector2(r - (_row / 2f) + 0.5f, c - (_column / 2f) + 0.5f), Quaternion.identity, transform);
+                switch (maze[i, j])
+                {
+                    case 0:
+                        cell = _wallTile;
+                        cell.State = CellState.CannotOpen;
+                        cell.gameObject.GetComponent<SpriteRenderer>().color = Color.black;
+                        break;
+                    case 1:
+                        cell = _floorTile;
+                        cell.State = CellState.None;
+                        cell.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+                        break;
+                }
+                Instantiate(cell, new Vector2
+                        (i - maze.GetHeight() / 2f + 0.5f, j - maze.GetWidth() / 2f + 0.5f), Quaternion.identity, transform);
             }
         }
     }
