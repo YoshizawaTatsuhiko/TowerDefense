de2fd2c27a6427567b53716dba85fe7c963b179a
tatsupiko <identity2022@outlook.jp>
2023-08-16 21:39:40 +0900

    [Update] コードのリファクタリングをした。 [Fix] 生成したCellが全てWallになってしまう不具合を修正した。

--- Assets/GridMapController.cs
+++ Assets/GridMapController.cs
@@ -7,86 +7,55 @@ public class GridMapController : MonoBehaviour
     [SerializeField] private int _row = 5;
     [SerializeField] private int _column = 5;
     [SerializeField] private Cell _wallTile = null;
-    [SerializeField] private Cell _floorTile = null;
+    [SerializeField] private Cell _pathTile = null;
     private Cell[,] _cells = null;
-    HoleDigging _maze = null;
 
     private void Start()
     {
-        _maze = new HoleDigging(_row, _column);
-        _cells = new Cell[_maze.GetMazeWidth(), _maze.GetMazeHeight()];
-        CellInitializes(_maze.Get2DArray());
+        SetCell(ref _wallTile, CellState.CannotOpen, Color.black);
+        SetCell(ref _pathTile, CellState.None, Color.white);
 
-        for (int i = 0; i < _maze.GetMazeWidth(); i++)
+        var maze = new HoleDigging(_row, _column);
+        _cells = new Cell[maze.GetMazeWidth(), maze.GetMazeHeight()];
+        var blueprint = maze.Get2DArray();
+        CellsInit(ref blueprint);
+
+        for (int r = 0; r < _cells.GetLength(0); r++)
         {
-            for (int j = 0; j < _maze.GetMazeHeight(); j++)
+            for (int c = 0; c < _cells.GetLength(1); c++)
             {
-                Instantiate(_cells[i, j], new Vector2
-                    (i - _maze.GetMazeWidth() / 2f + 0.5f, 
-                     j - _maze.GetMazeHeight() / 2f + 0.5f), Quaternion.identity, transform);
+                Instantiate(_cells[r, c], new Vector2
+                    (r - _cells.GetLength(0) / 2f + 0.5f, 
+                     c - _cells.GetLength(1) / 2f + 0.5f), Quaternion.identity, transform);
             }
         }
     }
 
-    private void CellInitializes(int[,] blueprint)
+    private void CellsInit(ref int[,] blueprint)
     {
-        for (int i = 0; i < _cells.GetLength(0); i++)
-        {
-            for (int j = 0; j < _cells.GetLength(1); j++)
+        for (int r = 0; r < _cells.GetLength(0); r++)
+            for (int c = 0; c < _cells.GetLength(1); c++)
             {
-                CellInitialize(blueprint, i, j);
+                CellInit(ref blueprint, r, c);
             }
-        }
     }
 
-    private void CellInitialize(int[,] blueprint, int i, int j)
-    {
-        if (!TryGetCell(i, j, out Cell cell)) return;
-
-        switch (blueprint[i, j])
-        {
-            case 0:
-                cell = _wallTile;
-                cell.State = CellState.CannotOpen;
-                cell.gameObject.GetComponent<SpriteRenderer>().color = Color.black;
-                break;
-            case 1:
-                cell = _floorTile;
-                cell.State = CellState.None;
-                cell.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
-                break;
-        }
-        _cells[i, j] = cell;
-    }
-
-    //private void GetStartAndGoal(out (int, int) start, out (int, int) goal)
-    //{
-    //    start = (-1, -1);
-    //    goal = (-1, -1);
-
-    //    for (int r = 0; r < _cells.GetLength(0); r++)
-    //    {
-    //        for (int c = 0; c < _cells.GetLength(1); c++)
-    //        {
-    //            if (_cells[r, c].State == CellState.Start)
-    //            {
-    //                start = (r, c);
-    //            }
-    //            else if (_cells[r, c].State == CellState.Goal)
-    //            {
-    //                goal = (r, c);
-    //            }
-
-    //            if (start != (-1, -1) && goal != (-1, -1)) return;
-    //        }
-    //    }
-    //}
-
-    private void SetCellState(int r, int c, CellState state)
+    private void CellInit(ref int[,] blueprint, int r, int c)
     {
         if (!TryGetCell(r, c, out Cell cell)) return;
 
+        _cells[r, c] = blueprint[r, c] switch
+        {
+            0 => _wallTile,
+            1 => _pathTile,
+            _ => throw new System.Exception("Caseにない値が検出されました。"),
+        };
+    }
+
+    private void SetCell(ref Cell cell, CellState state, Color color)
+    {
         cell.State = state;
+        if (cell.TryGetComponent(out SpriteRenderer renderer)) renderer.color = color;
     }
 
     private bool TryGetCell(int r, int c, out Cell cell)
