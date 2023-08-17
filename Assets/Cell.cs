8117512931042ace24528d54b61b61c2844fc8f7
tatsupiko <identity2022@outlook.jp>
2023-08-17 22:39:21 +0900

    [Update] 実コストor推定コストが変更されたら、総コストも更新されるようにした。

--- Assets/Cell.cs
+++ Assets/Cell.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 // 日本語対応
 public class Cell : MonoBehaviour
 {
-    public Cell Parent { get; set; } = null;
-    public float TotalCost { get; set; } = 0f;  // 実コスト + 推定コスト
-    public float ActualCost { get; set; } = 0f;  // 実コスト
-    public float HeuristicCost { get; set; } = 0f;  // 推定コスト
+    public Vector2 CellVector { get; set; } = Vector2.zero;
+    public Cell Parent { get; set; } = null;  // 親ノード
+    public float TotalCost => _actualCost + _heuristicCost;  // 実コスト + 推定コスト
+    public float ActualCost { get => _actualCost; set => _actualCost = value; }  // 実コスト
+    public float HeuristicCost { get => _heuristicCost; set => _heuristicCost = value; }  // 推定コスト
     public CellState State { get => _cellState; set => _cellState = value; }  // セルの状態
 
     [SerializeField ] private CellState _cellState = CellState.None;
+    private float _actualCost = 0f;
+    private float _heuristicCost = 0f;
 }
 
 public enum CellState
