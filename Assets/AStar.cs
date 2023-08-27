705ef7b182a855abc7d5358258972f8796274f04
tatsupiko <identity2022@outlook.jp>
2023-08-27 22:36:14 +0900

    [Update] CellのStateを変更する関数を作成した。

--- Assets/AStar.cs
+++ Assets/AStar.cs
@@ -1,18 +1,42 @@
 using System;
-using System.Collections.Generic;
 
 // 日本語対応
-public class AStar<T>
+public class AStar
 {
     private Cell[,] _aStarMap = null;
+    private (int, int) _start = (0, 0);
+    private (int, int) _goal = (0, 0);
 
-    public AStar(T[,] map, (int, int) start, (int, int) goal)
+    public AStar(Cell[,] map, (int, int) start, (int, int) goal)
     {
-        _aStarMap = new Cell[map.GetLength(0), map.GetLength(1)];
+        _aStarMap = map;
+        _start = start;
+        _goal = goal;
+    }
+
+    private void ChengeCellState(int a, int b, CellState state)
+    {
+        if (_aStarMap[a, b].State != state) _aStarMap[a, b].State = state;
+    }
+
+    private void ChengeCellState(ref Cell cell, CellState state)
+    {
+        if (cell.State != state) cell.State = state;
     }
 
     private float CalcDistance(int a1, int a2, int b1, int b2)
     {
         return MathF.Sqrt((a1 - b1) * (a1 - b1) + (a2 - b2) * (a2 - b2));
+    }
+
+    private bool TryGetCell(int r, int c, out Cell cell)
+    {
+        if (r < 0 || r >= _aStarMap.GetLength(0) || c < 0 || c >= _aStarMap.GetLength(1))
+        {
+            cell = null;
+            return false;
+        }
+        cell = _aStarMap[r, c];
+        return true;
     }
 }
