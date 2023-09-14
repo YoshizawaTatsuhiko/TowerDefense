19a287c6fdc5eadd741f1207b47cc25cc020df83
tatsupiko <identity2022@outlook.jp>
2023-09-14 22:11:56 +0900

    [Fix] スペルミスを修正した。

--- Assets/Cell.cs
+++ Assets/Cell.cs
@@ -4,15 +4,14 @@ public class Cell
     public int Row => _row;
     public int Column => _column;
     public Cell Parent { get; set; } = null;  // 親ノード
-    public bool IsWaldable => _isWaldable;  // このCellが通れるかどうか
+    public bool IsWalkable => _isWalkable;  // このCellが通れるかどうか
     public float TotalCost => ActualCost + HeuristicCost;  // 合計コスト = 実コスト + 推定コスト
     public float ActualCost { get; set; } = 0f;  // 実コスト = スタートからどのくらい進んだか
     public float HeuristicCost { get; set; } = 0f;  // 推定コスト = ゴールからどのくらい離れているか
-    public CellState State { get; set; } = CellState.None;  // セルの状態
 
     private int _row = 0;
     private int _column = 0;
-    private bool _isWaldable = false;
+    private bool _isWalkable = false;
 
     /// <summary>このCellに経路探索においての基本情報を入れる</summary>
     /// <param name="row">行番号</param>
@@ -22,7 +21,7 @@ public class Cell
     {
         _row = row;
         _column = column;
-        _isWaldable = isWalkable;
+        _isWalkable = isWalkable;
     }
 }
 
