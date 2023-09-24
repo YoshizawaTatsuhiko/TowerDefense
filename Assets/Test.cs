eaefd1f12135b5e6bc86934579176b533ff60567
tatsupiko <identity2022@outlook.jp>
2023-09-24 13:38:09 +0900

    [Update] 最短経路探索が機能することを確認した。但し、最短経路が見つからないときがある。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -25,8 +24,8 @@ public class Test : MonoBehaviour
     {
         _aStar = new AStar(_map.GetLength(0), _map.GetLength(1));
         ApplyMatching(in _map);
-        _start = GetPath();
-        _goal = GetPath();
+        _start = GetRandomPath();
+        _goal = GetRandomPath(_start.Row, _start.Column);
         List<Cell> shortestPath = _aStar.FindPath(_start.Row, _start.Column, _goal.Row, _goal.Column);
 
         if (shortestPath == null) throw new System.NullReferenceException();
@@ -47,30 +46,39 @@ public class Test : MonoBehaviour
     /// <exception cref="System.IndexOutOfRangeException"></exception>
     private void ApplyMatching(in int[,] map)
     {
+        Cell cell = null;
+
         for (int r = 0; r < _map.GetLength(0); r++)
             for (int c = 0; c < _map.GetLength(1); c++)
             {
-                _aStar[r, c] = map[r, c] switch
+                cell = map[r, c] switch
                 {
                     0 => Generate(_wall, r, c),
                     1 => Generate(_path, r, c),
                     _ => throw new System.IndexOutOfRangeException()
                 };
+                cell.Row = r;
+                cell.Column = c;
+                _aStar[r, c] = cell;
             }
     }
 
-    private T Generate<T>(T obj, int row, int column) where T : Object
-        => Instantiate(obj, SetCenter(row, column), Quaternion.identity, transform);
+    private T Generate<T>(T obj, int row, int column) where T : MonoBehaviour
+    {
+        var t = Instantiate(obj, SetCenter(row, column), Quaternion.identity, transform);
+        t.gameObject.name += $"({row}, {column})";
+        return t;
+    }
 
     private Vector2 SetCenter(int row, int column)
-        => new Vector2(row - _map.GetLength(0) / 2 + 0.5f, column - _map.GetLength(1) / 2 + 0.5f);
+        => new Vector2(column - _map.GetLength(1) / 2 + 0.5f, row - _map.GetLength(0) / 2 + 0.5f);
 
     private void ChangeCellColor(Cell cell, Color color)
     {
         if (cell.TryGetComponent(out SpriteRenderer renderer)) renderer.color = color;
     }
 
-    private Cell GetPath()
+    private Cell GetRandomPath(int row = -1, int column = -1)
     {
         int randomR = 0;
         int randomC = 0;
@@ -80,7 +88,7 @@ public class Test : MonoBehaviour
             randomR = Random.Range(0, _map.GetLength(0));
             randomC = Random.Range(0, _map.GetLength(1));
         }
-        while (!_aStar[randomR, randomC].IsWalkable);
+        while (!_aStar[randomR, randomC].IsWalkable && randomR == row && randomC == column);
 
         return _aStar[randomR, randomC];
     }
