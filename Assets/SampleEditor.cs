5fca1d00835772a814691dbbff2d2e4c919b39c3
tatsupiko <identity2022@outlook.jp>
2023-10-05 23:28:03 +0900

    [Add] Editor拡張のサンプルを作成してみた。

---
+++ Assets/SampleEditor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+// 日本語対応
+public class SampleEditor : EditorWindow
+{
+    private string _text = null;
+
+    [MenuItem("Sample/Custum Editor")]
+    private static void ShowWindow()
+    {
+        var editor = GetWindow<SampleEditor>();
+        editor.titleContent = new GUIContent("Sample");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("OUT PUT");
+        _text = EditorGUILayout.TextField(_text, GUILayout.Height(100));
+
+        if (GUILayout.Button("Write Down"))
+        {
+            Debug.Log(_text);
+        }
+    }
+}
