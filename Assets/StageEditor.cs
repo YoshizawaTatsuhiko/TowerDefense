a7718fc9e548287c50808a3af81573033b49a971
tatsupiko <identity2022@outlook.jp>
2023-10-09 23:57:09 +0900

    [Update] Editor拡張の進捗更新。

--- Assets/StageEditor.cs
+++ Assets/StageEditor.cs
@@ -8,11 +8,26 @@ namespace StageCreator
     // 日本語対応
     public class StageEditor : EditorWindow
     {
-        [MenuItem("Sample/Create")]
+        [MenuItem("Sample/Stage Create")]
         private static void Open()
         {
             var window = GetWindow<StageEditor>();
-            window.titleContent = new GUIContent("Stage Manager");
+            window.titleContent = new GUIContent("TD Stage Designer");
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Stage");
+            GUILayout.BeginHorizontal();
+            {
+                if (GUILayout.Button("Create"))
+                {
+                    Debug.Log("Create");
+                }
+            }
+            GUILayout.EndHorizontal();
         }
     }
+
+
 }
