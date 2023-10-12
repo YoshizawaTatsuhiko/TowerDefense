93ed77c5cdcb5f397ec1e94334aa911d308e227d
tatsupiko <identity2022@outlook.jp>
2023-10-12 23:56:36 +0900

    [FIx] クラスのコンストラクタで内部に持っている最短経路のリストをnewするようにした。

--- Assets/Script/Algorithm/AStarPathResult.cs
+++ Assets/Script/Algorithm/AStarPathResult.cs
@@ -3,5 +3,10 @@ using System.Collections.Generic;
 // 日本語対応
 public class AStarPathResult
 {
+    public AStarPathResult()
+    {
+        ShortestPath = new List<Cell>();
+    }
+
     public List<Cell> ShortestPath = null;
 }
