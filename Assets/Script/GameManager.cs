1f24aef91320996fef1ce02f9c4945839d559e9c
tatsupiko <identity2022@outlook.jp>
2023-10-15 22:56:25 +0900

    [Update] 特になし

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -7,7 +7,14 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance => _instance;
 
+    public event System.Action<EnemyController> EnemyAction 
+    { 
+        add => _enemyAction += value;
+        remove => _enemyAction -= value; 
+    }
+
     private static GameManager _instance;
+    private event System.Action<EnemyController> _enemyAction = null;
 
     private void Awake()
     {
