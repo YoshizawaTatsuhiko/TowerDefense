88605ef342f3046569d3c8948ad68c02f4d07e6b
tatsupiko <identity2022@outlook.jp>
2023-10-31 23:58:39 +0900

    [Update] 経路探索用の名前空間を追加した。

--- Assets/Editor/Stage.cs
+++ Assets/Editor/Stage.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using StageEditor;
+using PathFinding;
 
 // 日本語対応
 namespace StageEditor
--- Assets/Editor/StageEditor.cs
+++ Assets/Editor/StageEditor.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using PathFinding;
 
 // 日本語対応
 namespace StageEditor
