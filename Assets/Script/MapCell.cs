3535217b53850849ad5a285aeee1a6c274d2f338
tatsupiko <identity2022@outlook.jp>
2023-11-04 23:18:42 +0900

    [Update] 各プロパティにコメントを記載した。 [Update] 目的地の方向のベクトルを持つプロパティを作成した。

--- Assets/Script/MapCell.cs
+++ Assets/Script/MapCell.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 // 日本語対応
 public class MapCell : MonoBehaviour
 {
-    public int Row { get; private set; } = 0;
-    public int Column { get; private set; } = 0;
-    public float VisitedCost { get; private set; } = 0.0f;
+    public int Row { get; private set; } = 0;  // 行番号
+    public int Column { get; private set; } = 0;  // 列番号
+    public float VisitedCost { get; private set; } = 0.0f;  // このセルを訪れるのにかかるコスト
     public bool IsWalkable { get; private set; } = false;  // このCellが通れるかどうか
+    public Vector3 ToDestination { get; set; } = Vector3.zero;  // 目的地の方向
 
     public MapCell SetInfo(int row, int column, float cost, bool isWalkable)
     {
