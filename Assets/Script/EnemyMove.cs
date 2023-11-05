8ebf1130df275d1ac5e4d3579d09f3f117792e30
tatsupiko <identity2022@outlook.jp>
2023-11-05 23:34:10 +0900

    [Update] 敵が経路に沿って移動するコルーチンを作成した。（これからテストを行う）

--- Assets/Script/EnemyMove.cs
+++ Assets/Script/EnemyMove.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks.Triggers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,32 @@ public class EnemyMove : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 1.0f;
 
-    private void Move(Vector2 dest)
+    private IEnumerator AlongTheRoute(Vector3[] route)
     {
-        Vector2 myPos = transform.position;
-
-        if ((dest - myPos).magnitude <= 0.1f) return;
-
-        transform.position = (dest - myPos).normalized * _moveSpeed;
+        foreach (var dest in route)
+        {
+            yield return Move(dest);
+        }
     }
+
+    private IEnumerator Move(Vector3 dest)
+    {
+        Vector3 movementDir = dest - transform.position;
+
+        while (movementDir.magnitude > 0.1f)
+        {
+            transform.position += movementDir.normalized * _moveSpeed;
+            movementDir = dest - transform.position;
+            yield return null;
+        }
+    }
+
+    //private void Move(Vector2 dest)
+    //{
+    //    Vector2 myPos = transform.position;
+
+    //    if ((dest - myPos).magnitude <= 0.1f) return;
+
+    //    transform.position = (dest - myPos).normalized * _moveSpeed;
+    //}
 }
